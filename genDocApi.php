a02c983aa626324be8cdc02d610be101698377ef
Mehdy Dara <mdara@eleven-labs.com>
2014-05-19 11:43:24 +0200

    Update genDocApi.php example

--- genDocApi.php
+++ genDocApi.php
@@ -1,21 +1,31 @@
 <?php
 
-// Use your autoload
-// require __DIR__.'/vendor/autoload.php';
+// Use autoload
+require __DIR__.'/../../autoload.php';
 
-// Or include classe
-require_once(__DIR__.'/src/Builder.php');
-require_once(__DIR__.'/src/Extractor.php');
-require_once(__DIR__.'/../../../../../src/Controller/GetController.php');
+// Or include classes
+// require_once(__DIR__.'/src/Builder.php');
+// require_once(__DIR__.'/src/Extractor.php');
+// require_once(__DIR__.'/example/User.php');
+// require_once(__DIR__.'/example/Document.php');
 
 use Zckrs\GenDocApi\Builder;
 
+// Define classes to search annotations
 $classes = array(
-    'Controller\GetController',
+    'Example\User',
+    'Example\Document',
 );
 
 try {
-    $builder = new Builder($classes);
+    $builder = new Builder($classes,
+        array(
+            'output_file'  => 'index.html',
+            'output_dir'   => __DIR__.'/../web',
+            'template_dir' => __DIR__.'/Resources/views',
+            'asset_dir'    => __DIR__.'/Resources/assets',
+        )
+    );
     $builder->generate();
 } catch (\Exception $e) {
     echo 'There was an error generating the documentation: ', $e->getMessage();
