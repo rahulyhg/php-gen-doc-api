b964582accaf1cc27097d93c42ff088ea99cdca8
Mehdy Dara <mdara@eleven-labs.com>
2014-05-20 18:12:15 +0200

    Generate index.html with test class

--- genDocApi.php
+++ genDocApi.php
@@ -1,27 +1,25 @@
 <?php
 
 // Use autoload
-require __DIR__.'/../../autoload.php';
+// require __DIR__.'/../../autoload.php';
 
 // Or include classes
-// require_once(__DIR__.'/src/Builder.php');
-// require_once(__DIR__.'/src/Extractor.php');
-// require_once(__DIR__.'/example/User.php');
-// require_once(__DIR__.'/example/Document.php');
+require_once(__DIR__.'/src/Builder.php');
+require_once(__DIR__.'/src/Extractor.php');
+require_once(__DIR__.'/tests/sample/Client.php');
 
 use Zckrs\GenDocApi\Builder;
 
 // Define classes to search annotations
 $classes = array(
-    'Example\User',
-    'Example\Document',
+    'Zckrs\GenDocApi\Test\Client',
 );
 
 try {
     $builder = new Builder($classes,
         array(
             'output_file'  => 'index.html',
-            'output_dir'   => __DIR__.'/../web',
+            'output_dir'   => __DIR__.'/web',
             'template_dir' => __DIR__.'/Resources/views',
             'asset_dir'    => __DIR__.'/Resources/assets',
         )
